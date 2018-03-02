52a78ba4d16d467b72ca6055770414a50ead751b
Alex Kuznetsov <KuznetsovAlexander@rambler.ru>
2018-03-03 00:50:14 +0400

    Added ability to automatically generate parking info.
    Signed-off-by: Alex Kuznetsov <KuznetsovAlexander@rambler.ru>

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,13 +9,53 @@
 
 using namespace std;
 
+void fillParkingWithCars(std::vector<cv::Rect>& cars)
+{
+  ofstream outputFile("parkinglot_new.txt", std::ofstream::out | std::ofstream::trunc);
+  int i = 0;
+  for (auto car : cars) {
+    outputFile << i++ << " " 
+      << car.x << " " << car.y << " "
+      << car.x + car.width << " " << car.y << " "
+      << car.x + car.width << " " << car.y + car.height << " "
+      << car.x << " " << car.y + car.height << " "
+      << car.x + car.width / 2 << " "
+      << car.y + car.height / 2 << " "
+      << "0"
+      << endl;
+  }
+  outputFile.close();
+
+}
+
+void detectAndDisplay(cv::Mat frame)
+{
+  cv::CascadeClassifier cars_cascade; 
+  cars_cascade.load("datasets/haarcascades/cars.xml");
+
+  std::vector<cv::Rect> cars;
+  cv::Mat frame_gray;
+  cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+  equalizeHist(frame_gray, frame_gray);
+  //-- Detect cars
+  cars_cascade.detectMultiScale(frame_gray, cars, 1.1, 2);
+  fillParkingWithCars(cars);
+  for (size_t i = 0; i < cars.size(); i++) {
+    cv::Point center(cars[i].x + cars[i].width / 2, cars[i].y + cars[i].height / 2);
+    cv::ellipse(frame, center, cv::Size(cars[i].width / 2, cars[i].height / 2), 0, 0, 360, cv::Scalar(255, 0, 255), 4, 8, 0);
+  }
+  //-- Show what you got
+  cv::imshow("Video", frame);
+}
+
 int main(int argc, char** argv)
 {
-  if (argc != 3)
+  if (argc < 3)
   {
-    printf("usage: DetectParking.exe <Video_Filename or Camera_Number> <ParkingData_Filename>\n\n");
+    printf("usage: DetectParking.exe <Video_Filename or Camera_Number> <ParkingData_Filename> <-f>\n\n");
     printf("<Camera_Number> can be 0, 1, ... for attached cameras\n");
     printf("<ParkingData_Filename> should be simple txt file with lines of: id x1 y1 x2 y2 x3 y3 x4 y4\n");
+    printf("<-f> says that program will generate ParkingData file\n");
     return -1;
   }
 
@@ -25,6 +65,9 @@ int main(int argc, char** argv)
   const string videoFilename = argv[1];
   vector<Parking> parking_data = parse_parking_file(argv[2]);
   const int delay = 1;
+
+  string argv3 = (argc > 3) ? argv[3] : "";
+  bool findParkingPlaces = (argv3 == "-f");
 
   // Open Camera or Video	File
   cv::VideoCapture cap;
@@ -80,71 +123,73 @@ int main(int argc, char** argv)
       return -1;
     }
 
-    // unused?
-    //double video_pos_msec = cap.get(cv::CAP_PROP_POS_MSEC);
-
     double video_pos_frame = cap.get(cv::CAP_PROP_POS_FRAMES);
 
     frame_out = frame.clone();
-    cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
-    cv::GaussianBlur(frame_gray, frame_blur, blur_kernel, 3, 3);
 
-    cout << ConfigLoad::options["DETECT_PARKING"];
-    printf("\n");
+    if (findParkingPlaces) {
+      detectAndDisplay(frame_out);
+    } else {
+      cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+      cv::GaussianBlur(frame_gray, frame_blur, blur_kernel, 3, 3);
 
-    if (ConfigLoad::options["DETECT_PARKING"] == "true")
-    {
-      for (Parking& park : parking_data)
-      {
-        // Check if parking is occupied
-        roi = frame_blur(park.getBoundingRect());
-        cv::Laplacian(roi, laplacian, CV_64F);
-        delta = cv::mean(cv::abs(laplacian), park.getMask());
-        park.setStatus(delta[0] < atof(ConfigLoad::options["PARK_LAPLACIAN_TH"].c_str()));
-        printf("| %d: d=%-5.1f", park.getId(), delta[0]);
-      }
+      cout << ConfigLoad::options["DETECT_PARKING"];
       printf("\n");
-    }
 
-    // Parking Overlay
-    for (Parking park : parking_data)
-    {
-      if (park.getStatus())  color = cv::Scalar(0, 255, 0);
-      else color = cv::Scalar(0, 0, 255);
-      cv::drawContours(frame_out, park.getContourPoints(), -1, color, 2, cv::LINE_AA);
-
-      // Parking ID overlay
-      cv::Point p = park.getCenterPoint();
-      cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x + 1, p.y + 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
-      cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x - 1, p.y - 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
-      cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x - 1, p.y + 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
-      cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x + 1, p.y - 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
-      cv::putText(frame_out, to_string(park.getId()), p, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 2, cv::LINE_AA);
-    }
-    
-    double time = clock();
-    if (lastUpdateTime == 0 || time - lastUpdateTime > 1000) {
-      lastUpdateTime = time;
-      ofstream outputFile("parking.txt", std::ofstream::out | std::ofstream::trunc);
-      for (Parking park : parking_data) { 
-        outputFile << park.getId() << " " << park.getStatus() << endl;
+      if (ConfigLoad::options["DETECT_PARKING"] == "true")
+      {
+        for (Parking& park : parking_data)
+        {
+          // Check if parking is occupied
+          roi = frame_blur(park.getBoundingRect());
+          cv::Laplacian(roi, laplacian, CV_64F);
+          delta = cv::mean(cv::abs(laplacian), park.getMask());
+          park.setStatus(delta[0] < atof(ConfigLoad::options["PARK_LAPLACIAN_TH"].c_str()));
+          printf("| %d: d=%-5.1f", park.getId(), delta[0]);
+        }
+        printf("\n");
       }
-      outputFile.close();
-    }
 
-    // Text Overlay
-    oss.str("");
-    oss << (unsigned long int)video_pos_frame << "/" << total_frames;
-    cv::putText(frame_out, oss.str(), cv::Point(5, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 255), 2, cv::LINE_AA);
+      // Parking Overlay
+      for (Parking park : parking_data)
+      {
+        if (park.getStatus())  color = cv::Scalar(0, 255, 0);
+        else color = cv::Scalar(0, 0, 255);
+        cv::drawContours(frame_out, park.getContourPoints(), -1, color, 2, cv::LINE_AA);
 
-    // Save Video
-    if (ConfigLoad::options["SAVE_VIDEO"] == "true")
-    {
-      outputVideo.write(frame_out);
+        // Parking ID overlay
+        cv::Point p = park.getCenterPoint();
+        cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x + 1, p.y + 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
+        cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x - 1, p.y - 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
+        cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x - 1, p.y + 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
+        cv::putText(frame_out, to_string(park.getId()), cv::Point(p.x + 1, p.y - 1), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);
+        cv::putText(frame_out, to_string(park.getId()), p, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 2, cv::LINE_AA);
+      }
+
+      double time = clock();
+      if (lastUpdateTime == 0 || time - lastUpdateTime > 1000) {
+        lastUpdateTime = time;
+        ofstream outputFile("parking.txt", std::ofstream::out | std::ofstream::trunc);
+        for (Parking park : parking_data) {
+          outputFile << park.getId() << " " << park.getStatus() << endl;
+        }
+        outputFile.close();
+      }
+
+      // Text Overlay
+      oss.str("");
+      oss << (unsigned long int)video_pos_frame << "/" << total_frames;
+      cv::putText(frame_out, oss.str(), cv::Point(5, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 255), 2, cv::LINE_AA);
+
+      // Save Video
+      if (ConfigLoad::options["SAVE_VIDEO"] == "true")
+      {
+        outputVideo.write(frame_out);
+      }
+      cv::imshow("Video", frame_out);
     }
 
     // Show Video
-    cv::imshow("Video", frame_out);
     c = (char)cv::waitKey(delay);
     if (c == 27) break;
   }
